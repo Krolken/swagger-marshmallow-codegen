4629046969dc51e2b6bcc752756adaefff00d98e
podhmo <ababjam61+github@gmail.com>
2017-10-28 02:31:45 +0900

    support include_self (todo test)

--- swagger_marshmallow_codegen/schema/composed.py
+++ swagger_marshmallow_codegen/schema/composed.py
@@ -18,20 +18,43 @@ class ComposedOpts(SchemaOpts):
                 raise ValueError("`discriminator` must need `fieldname` value")
 
 
+def _detect_include_self(schema_classes):
+    include_self = False
+    new_schema_classes = []
+    for c in schema_classes:
+        if c == "self":
+            include_self = True
+            continue
+        new_schema_classes.append(c)
+    return new_schema_classes, include_self
+
+
 class OneOfSchema(Schema):
     OPTIONS_CLASS = ComposedOpts
     schema_classes = None
+    include_self = None
 
     def __init__(self, *args, **kwargs):
         strict = kwargs.pop("strict", None)
         many = kwargs.pop("many", None)
+
+        # xxx: class level
+        if self.__class__.include_self is None:
+            cls = self.__class__
+            cls.schema_classes, cls.include_self = _detect_include_self(cls.schema_classes)
+
         schema_classes = self.opts.schema_classes or self.__class__.schema_classes
+
         self.schemas = [cls(*args, strict=False, many=False, **kwargs) for cls in schema_classes]
         super().__init__(strict=strict, many=many)
 
         finder = SchemaFinder(self.schemas, self.opts.discriminator)
-        self._marshal = ComposedMarshaller(self._marshal, finder, self.final_check)
-        self._unmarshal = ComposedUnmarshaller(self._unmarshal, finder, self.final_check)
+        self._marshal = ComposedMarshaller(
+            self._marshal, finder, self.final_check, include_self=self.__class__.include_self
+        )
+        self._unmarshal = ComposedUnmarshaller(
+            self._unmarshal, finder, self.final_check, include_self=self.__class__.include_self
+        )
 
     def final_check(self, *, data, schemas, results, errors, compacted):
         if len(compacted) == 1:
@@ -50,9 +73,11 @@ class OneOfSchema(Schema):
         else:
             for other in results[1:]:
                 results[0].update(other)
+            candidates = [s.__class__.__name__ for s in self.schemas]
+            if self.__class__.include_self:
+                candidates.append("self")
             return data if not results else results[0], {
-                "_schema":
-                ["not matched, any of {}".format([s.__class__.__name__ for s in self.schemas])]
+                "_schema": ["not matched, any of {}".format(candidates)],
             }
 
 
@@ -138,11 +163,12 @@ class SchemaFinder:
 
 
 class ComposedMarshaller(marshalling.Marshaller):
-    def __init__(self, marshaller, finder, final_check):
+    def __init__(self, marshaller, finder, final_check, *, include_self=False):
         super().__init__()
         self._marshaller = marshaller
         self.finder = finder
         self.final_check = final_check
+        self.include_self = include_self
 
     def marshall(self, obj, fields_dict, *, many, accessor, dict_class, index_errors, index=None):
         self.reset_errors()
@@ -158,7 +184,7 @@ class ComposedMarshaller(marshalling.Marshaller):
                 index=index,
             )
         except ValidationError as err:
-            self_errors = self._marshal.errors
+            self_errors = self._marshaller.errors
             result = err.data
 
         if many:
@@ -168,11 +194,17 @@ class ComposedMarshaller(marshalling.Marshaller):
         else:
             d, errors = self._marshall_one(obj)
             d.update(result)
-        if self_errors is not None:
-            errors.update(self_errors)
-        self.errors = errors
-        if errors:
-            raise ValidationError(errors, data=d)
+
+        if not self.include_self:
+            if self_errors is not None:
+                errors.update(self_errors)
+            self.errors = errors
+        else:
+            self.errors = _merge_errors_for_include_self(
+                errors, self_errors, many=many, size=len(result) if many else 1
+            )
+        if self.errors:
+            raise ValidationError(self.errors, data=d)
         return d
 
     __call__ = marshall
@@ -194,11 +226,12 @@ class ComposedMarshaller(marshalling.Marshaller):
 
 
 class ComposedUnmarshaller(marshalling.Unmarshaller):
-    def __init__(self, unmarshaller, finder, final_check):
+    def __init__(self, unmarshaller, finder, final_check, *, include_self=False):
         super().__init__()
         self._unmarshaller = unmarshaller
         self.finder = finder
         self.final_check = final_check
+        self.include_self = include_self
 
     def unmarshall(self, data, fields, *, many, partial, dict_class, index_errors):
         self.reset_errors()
@@ -223,9 +256,15 @@ class ComposedUnmarshaller(marshalling.Unmarshaller):
         else:
             d, errors = self._unmarshall_one(data, partial=partial)
             d.update(result)
-        if self_errors is not None:
-            errors.update(self_errors)
-        self.errors = errors
+
+        if not self.include_self:
+            if self_errors is not None:
+                errors.update(self_errors)
+            self.errors = errors
+        else:
+            self.errors = _merge_errors_for_include_self(
+                errors, self_errors, many=many, size=len(result) if many else 1
+            )
         return d
 
     __call__ = unmarshall
@@ -244,3 +283,28 @@ class ComposedUnmarshaller(marshalling.Unmarshaller):
         return self.final_check(
             data=data, results=results, errors=errors, schemas=schemas, compacted=compacted
         )
+
+
+def _merge_errors_for_include_self(errors, self_errors, *, many, size):
+    if many:
+        for i in range(size):
+            if i in errors:
+                errors[i] = _merge_errors_one_for_include_self(
+                    errors[i],
+                    self_errors if self_errors is None else self_errors.get(i),
+                )
+        return errors
+    else:
+        return _merge_errors_one_for_include_self(errors, self_errors)
+
+
+def _merge_errors_one_for_include_self(errors, self_errors):
+    if self_errors is not None:
+        if not all(msg.startswith("not matched, any of") for msg in errors.get("_schema") or []):
+            errors.update(self_errors)
+    else:
+        if not errors:
+            errors.update(_schema=["satisfied both of self and others, not only one"])
+        elif all(msg.startswith("not matched, any of") for msg in errors.get("_schema") or []):
+            errors.pop("_schema")
+    return errors
